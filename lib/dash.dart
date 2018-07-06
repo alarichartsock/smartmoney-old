9131fbe99ad46dcd22d8ba22fdb4f63b56907ee3
ArtificialPenguins <alaric.hartsock@yahoo.com>
2018-07-06 15:40:21 -0700

    parameterized stuff
    i've turned all of the hardcoded mock data into parameterized classes, as well as parameterized the screen. todo: create the animation to go from the screen to the dash.

--- lib/dash.dart
+++ lib/dash.dart
@@ -6,6 +6,9 @@ class Dash extends StatefulWidget {
 }
 
 class _DashState extends State<Dash> {
+
+  var activeScreen = Home;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,7 +24,7 @@ class _DashState extends State<Dash> {
             backgroundColor: Colors.transparent,
             elevation: 0.0,
             title: Text(
-              "moves",
+              activeScreen.title,
               style: TextStyle(
                   fontSize: 27.5,
                   fontWeight: FontWeight.w700,
@@ -44,223 +47,147 @@ class _DashState extends State<Dash> {
               )
             ],
           ),
-          body: ListView(
+          body: activeScreen.contentBuilder(context),
+        )
+        );
+  }
+}
+
+class Screen {
+  final String title;
+  final WidgetBuilder contentBuilder;
+
+  Screen({
+    this.title,
+    this.contentBuilder
+  });
+}
+
+final Screen Home = new Screen(
+  title: "Watchlist",
+  contentBuilder: (BuildContext context) {
+    return ListView(
             children: [
-              Card(
-                elevation: 10.0,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/fake_chart_1.jpg',
-                      width: double.infinity,
-                      height: 160.0,
-                      fit: BoxFit.cover,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.attach_money,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 2.5),
-                                child: Text(
-                                  r"FB 150$ Put",
-                                  style: TextStyle(
-                                      fontSize: 28.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
-                                child: Text(
-                                  "7.2M @11:20AM PST, 7/20 exp",
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40.0, 10.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.add,
-                              color: Colors.white,
-                              //size: 20.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+              StockCard(
+                headImageAssetPath: 'assets/images/fake_chart_1.jpg',
+                iconBackgroundColor: Colors.green,
+                ticker: 'FB',
+                strikePrice: '150',
+                type: 'Put',
+                volume: '7.1',
+                time: '12:11 AM PST',
+                exp: '7/20',
+            ), StockCard(
+                headImageAssetPath: 'assets/images/fake_chart_1.jpg',
+                iconBackgroundColor: Colors.green,
+                ticker: 'FB',
+                strikePrice: '150',
+                type: 'Put',
+                volume: '7.1',
+                time: '12:11 AM PST',
+                exp: '7/20',
+            ), StockCard(
+                headImageAssetPath: 'assets/images/fake_chart_1.jpg',
+                iconBackgroundColor: Colors.green,
+                ticker: 'FB',
+                strikePrice: '150',
+                type: 'Put',
+                volume: '7.1',
+                time: '12:11 AM PST',
+                exp: '7/20',
+            )],
+          );
+  }
+);
+
+class StockCard extends StatelessWidget {
+
+  final String headImageAssetPath;
+  final Color iconBackgroundColor;
+  final String ticker;
+  final String strikePrice;
+  final String type;
+  final String volume;
+  final String time;
+  final String exp;
+
+  StockCard({
+    this.headImageAssetPath,
+    this.iconBackgroundColor,
+    this.ticker,
+    this.strikePrice,
+    this.type,
+    this.volume,
+    this.time,
+    this.exp
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10.0,
+      child: Column(
+        children: <Widget>[
+          Image.asset(
+            headImageAssetPath,
+            width: double.infinity,
+            height: 160.0,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 10.0),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: iconBackgroundColor,
+                    borderRadius: BorderRadius.all(const Radius.circular(15.0)),
+                  ),
+                  child: Icon(
+                    Icons.attach_money,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              Card(
-                elevation: 10.0,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/fake_chart_2.jpg',
-                      width: double.infinity,
-                      height: 160.0,
-                      fit: BoxFit.cover,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.attach_money,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 2.5),
-                                child: Text(
-                                  r"MU 50$ Put",
-                                  style: TextStyle(
-                                      fontSize: 28.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
-                                child: Text(
-                                  "3.2M @2:40PM PST, 9/40 exp",
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40.0, 10.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.add,
-                              color: Colors.white,
-                              //size: 20.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 2.5),
+                      child: Text(
+                          "$ticker $strikePrice" + r"$ " + "$type",
+                        style: TextStyle(
+                            fontSize: 28.0, fontWeight: FontWeight.w600),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+                      child: Text(
+                        "$volume M $time, $exp exp",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w600),
+                      )),
+                ],
               ),
-              Card(
-                elevation: 10.0,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/fake_chart_3.jpg',
-                      width: double.infinity,
-                      height: 160.0,
-                      fit: BoxFit.cover,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.attach_money,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 2.5),
-                                child: Text(
-                                  r"IQ 150$ Call",
-                                  style: TextStyle(
-                                      fontSize: 28.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Padding(
-                                padding:
-                                    EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
-                                child: Text(
-                                  "7.2M @9:20AM PST, 7/15 exp",
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(40.0, 10.0, 0.0, 10.0),
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius:
-                                  BorderRadius.all(const Radius.circular(15.0)),
-                            ),
-                            child: Icon(
-                              Icons.add,
-                              color: Colors.white,
-                              //size: 20.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+              Padding(
+                padding: EdgeInsets.fromLTRB(40.0, 10.0, 0.0, 10.0),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(const Radius.circular(15.0)),
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    //size: 20.0,
+                  ),
                 ),
               ),
             ],
-          ),
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
